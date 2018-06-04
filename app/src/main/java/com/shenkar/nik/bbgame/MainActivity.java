84bdeedd6445459b9a0b35b80c09bf7bb4bd2e67
DESKTOP-8L190A9\nik <nbron86@gmail.com>
2018-06-04 20:17:13 +0200

    added music

--- app/src/main/java/com/shenkar/nik/bbgame/MainActivity.java
+++ app/src/main/java/com/shenkar/nik/bbgame/MainActivity.java
@@ -1,13 +1,38 @@
 package com.shenkar.nik.bbgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button soundButton;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        soundButton = (Button) findViewById(R.id.sound);
+        playButton = (Button) findViewById(R.id.play);
+
+        playButton.setOnClickListener(this);
+        soundButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.play:
+                Intent intent = new Intent(this,bbgActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.sound:
+                Intent intent2 = new Intent(this,soundActivity.class);
+                startActivity(intent2);
+                break;
+        }
     }
 }
--- app/src/main/java/com/shenkar/nik/bbgame/bbgActivity.java
+++ app/src/main/java/com/shenkar/nik/bbgame/bbgActivity.java
@@ -1,13 +1,28 @@
 package com.shenkar.nik.bbgame;
 
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class bbgActivity extends Activity {
-
+    MediaPlayer mMediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_bbg);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bbg);
+
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer = MediaPlayer.create(this,R.raw.song);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setLooping(true);
+        mMediaPlayer.start();
+    }
+    protected void onDestroy() {
+        //other codes
+        super.onDestroy();
+        mMediaPlayer.stop();
     }
 }
