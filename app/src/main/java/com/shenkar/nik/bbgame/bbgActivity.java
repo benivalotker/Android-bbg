0842177cfca6490c7e628f3bcf623bf07dcf0768
DESKTOP-8L190A9\nik <nbron86@gmail.com>
2018-06-04 20:07:54 +0200

    added game activity and sound activity

---
+++ app/src/main/java/com/shenkar/nik/bbgame/bbgActivity.java
@@ -0,0 +1,13 @@
+package com.shenkar.nik.bbgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class bbgActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bbg);
+    }
+}
