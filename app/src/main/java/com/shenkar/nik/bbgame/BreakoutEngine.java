894185b02f9e15fc5d89f30ffd2d75d09cddedca
nikita <nbron86@gmail.com>
2018-06-05 22:33:36 +0200

    added BreakoutEngine to project

---
+++ app/src/main/java/com/shenkar/nik/bbgame/BreakoutEngine.java
@@ -0,0 +1,20 @@
+package com.shenkar.nik.bbgame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class BreakoutEngine extends SurfaceView {
+
+    public BreakoutEngine(Context context) {
+        super(context);
+    }
+
+    public void pause(){
+
+    }
+
+    public void resume(){
+
+    }
+
+}
