32397f2ade113a545019a1c178e0410e625228ae
nikita-br <nbron86@gmail.com>
2018-06-05 09:14:36 -0700

    changed icons size + removed title bar from style.xml

--- app/src/main/java/com/shenkar/nik/bbgame/soundActivity.java
+++ app/src/main/java/com/shenkar/nik/bbgame/soundActivity.java
@@ -24,6 +24,7 @@ public class soundActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
 
