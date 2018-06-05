8221dbba76d888a97eb6b5d083f0b68fbfbc7f81
nikita-br <nbron86@gmail.com>
2018-06-05 07:37:07 -0700

    added mute and unmute sound icons

--- app/src/main/java/com/shenkar/nik/bbgame/soundActivity.java
+++ app/src/main/java/com/shenkar/nik/bbgame/soundActivity.java
@@ -1,10 +1,14 @@
 package com.shenkar.nik.bbgame;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -14,6 +18,8 @@ public class soundActivity extends AppCompatActivity {
     SeekBar seekbar;
     AudioManager audioManager;
     TextView textview;
+    ImageView imageView;
+    ImageView imageView2;
 
 
     @Override
@@ -21,17 +27,43 @@ public class soundActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
 
-
+        imageView = (ImageView)findViewById(R.id.mute) ;
+        imageView2 = (ImageView)findViewById(R.id.unMute) ;
         textview = (TextView)findViewById(R.id.textView1);
         seekbar = (SeekBar)findViewById(R.id.seekBar);
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         seekbar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
 
+        imageView.setColorFilter(Color.RED, PorterDuff.Mode.MULTIPLY);
+        imageView2.setColorFilter(Color.BLUE, PorterDuff.Mode.MULTIPLY);
+        //imageView.setVisibility(View.INVISIBLE);
+
+        int volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        seekbar.setProgress(volume);
+        if(volume==0){
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+        }
+        else {
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+        }
+
+
+
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
 
-                textview.setText("Media Volume : " + i);
+                textview.setText("Volume : " + i);
+                if(i==0){
+                    imageView.setVisibility(View.VISIBLE);
+                    imageView2.setVisibility(View.INVISIBLE);
+                }
+                else if(i>0){
+                    imageView.setVisibility(View.INVISIBLE);
+                    imageView2.setVisibility(View.VISIBLE);
+                }
 
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
             }
