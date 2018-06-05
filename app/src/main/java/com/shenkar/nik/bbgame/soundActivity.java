a718fe10123d21ec339746989816a1ed2768bb6a
nikita-br <nbron86@gmail.com>
2018-06-05 06:30:05 -0700

    added seekbar in sound activity

--- app/src/main/java/com/shenkar/nik/bbgame/soundActivity.java
+++ app/src/main/java/com/shenkar/nik/bbgame/soundActivity.java
@@ -1,13 +1,62 @@
 package com.shenkar.nik.bbgame;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class soundActivity extends AppCompatActivity {
+
+    MediaPlayer mMediaPlayer;
+    SeekBar seekbar;
+    AudioManager audioManager;
+    TextView textview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sound);
+
+
+        textview = (TextView)findViewById(R.id.textView1);
+        seekbar = (SeekBar)findViewById(R.id.seekBar);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        seekbar.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                textview.setText("Media Volume : " + i);
+
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer = MediaPlayer.create(this,R.raw.song);
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setLooping(true);
+        mMediaPlayer.start();
+
+    }
+    protected void onDestroy() {
+        //other codes
+        super.onDestroy();
+        mMediaPlayer.stop();
     }
 }
