1915274c24786cc462daf057dec75457d8fecbfe
benivalotker <valotkerbeni@gmail.com>
2018-08-03 19:45:13 +0300

    pause dialog box - button interactive

--- app/src/main/java/com/shenkar/nik/bbgame/Dialog.java
+++ app/src/main/java/com/shenkar/nik/bbgame/Dialog.java
@@ -1,28 +1,44 @@
 package com.shenkar.nik.bbgame;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Dialog extends AppCompatActivity {
+
+    Button startOver, exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        startOver = (Button) findViewById(R.id.btn_startOver);
+        startOver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(Dialog.this, bbgActivity.class);
+                startActivity(intent);
             }
-        });*/
+        });
+
+        exit = (Button) findViewById(R.id.btn_exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dialog.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
