d1e33f5c8af6604d802aedc12b89eaff6f231b0d
benivalotker <valotkerbeni@gmail.com>
2018-08-13 21:41:13 +0300

    splash animation activity - test gif image

---
+++ app/src/main/java/com/shenkar/nik/bbgame/Splash.java
@@ -0,0 +1,45 @@
+package com.shenkar.nik.bbgame;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class Splash extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Thread timer = new Thread() {
+            public void run(){
+                try{
+                    sleep(7000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timer.start();
+
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+
+
+}
