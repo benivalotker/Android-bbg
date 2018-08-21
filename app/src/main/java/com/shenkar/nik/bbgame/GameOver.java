0035f4cbb0c3802f0d6d90f3f607a068fdadd6ee
benivalotker <valotkerbeni@gmail.com>
2018-08-21 20:28:08 +0300

    game over - landscape + full screen

--- app/src/main/java/com/shenkar/nik/bbgame/GameOver.java
+++ app/src/main/java/com/shenkar/nik/bbgame/GameOver.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class GameOver extends AppCompatActivity implements View.OnClickListener {
@@ -13,6 +14,9 @@ public class GameOver extends AppCompatActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
